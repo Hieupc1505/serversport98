519b115a4d22c764efa1e6bb096cfdfc63fce840
hieusky <vuhieukenny2@gmail.com>
2022-09-22 14:41:58 +0700

    change pakage to netlify

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    console.log(err.message);
+    console.log(err);
     res.status(err.status || 500)
         .json({
             sucess: false,
